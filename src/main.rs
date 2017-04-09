1458d653fe9fd8aed25ed59d41dcd9ad7136d088
Chang-jae Lee <cjlee@Chang-jaeui-MacBook-Pro.local>
2017-04-09 17:18:16 +0900

    add codes for reading twitter app key pairs file
    add codes for reading twitter app key pairs file, as String
    @TODO: read contents as JSON, then get each key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Read;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut inputfile = std::fs::File::open("Keys.json").unwrap();
+    let mut contents = String::from("");
+    match inputfile.read_to_string(&mut contents) {
+        Ok(n) => println!("Read {} lines", n),
+        Err(_) => println!("Cannot read file Keys.json"),
+    }
+
+    println!("contents:\n{}", contents);
 }
+
+
